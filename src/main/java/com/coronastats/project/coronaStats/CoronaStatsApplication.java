e288c55d62a1b47e2edaffdccc164ce766a392fb
AhmetY7 <ahmetylmz95@gmail.com>
2020-07-26 14:06:20 +0300

    Initializing project

---
+++ src/main/java/com/coronastats/project/coronaStats/CoronaStatsApplication.java
@@ -0,0 +1,13 @@
+package com.coronastats.project.coronaStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaStatsApplication.class, args);
+	}
+
+}
